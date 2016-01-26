2dd30c8cdb8395b36d2922c77d958f8c8ada916d
Aaron Schulz <aschulz@wikimedia.org>
2016-01-26 12:39:18 -0800

    Clean up INT fields
    Change-Id: I68e84fb9f6ffb8b41ac30921d34b6e1bee92f1a9

--- db/addReviewsTable.sql
+++ db/addReviewsTable.sql
@@ -1,13 +1,13 @@
 -- Add article assessments table
 
 CREATE TABLE IF NOT EXISTS /*_*/page_assessments (
-	pa_page_id          INT(10) NOT NULL,
+	pa_page_id          INT UNSIGNED NOT NULL,
 	pa_page_name        VARCHAR(255) NOT NULL,
-	pa_page_namespace   INT(11) NOT NULL,
+	pa_page_namespace   INT NOT NULL,
 	pa_project          VARCHAR(128) DEFAULT NULL,
 	pa_class            VARCHAR(20) DEFAULT NULL,
 	pa_importance       VARCHAR(20) DEFAULT NULL,
-	pa_page_revision    INT(10) NOT NULL
+	pa_page_revision    INT UNSIGNED NOT NULL
 )/*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/pa_project ON /*_*/ page_assessments (pa_project);
