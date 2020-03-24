c5bc27dd0729b15f8ec97ffdc8cc8945bf493cee
Reedy <reedy@wikimedia.org>
2020-03-24 00:56:21 +0000

    Move PRIMARY KEY definition in page_assessments_projects
    Bug: T248366
    Change-Id: I5c5729fdafa26005a15a69104ef42234f0693c1a

--- db/addProjectsTable.sql
+++ db/addProjectsTable.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS /*_*/page_assessments_projects (
   -- Generated ID of the project
-  pap_project_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  pap_project_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 
   -- Name of the project assessing the page. In the case of a subproject or task
   -- force, this will be a combination of the project and subproject name, e.g.
@@ -10,9 +10,7 @@ CREATE TABLE IF NOT EXISTS /*_*/page_assessments_projects (
   pap_project_title VARCHAR(255) DEFAULT NULL,
 
   -- ID of the parent project (for subprojects and task forces)
-  pap_parent_id INT UNSIGNED NULL DEFAULT NULL,
-
-  PRIMARY KEY (pap_project_id)
+  pap_parent_id INT UNSIGNED NULL DEFAULT NULL
 )/*$wgDBTableOptions*/;
 
 CREATE UNIQUE INDEX /*i*/pap_project_title ON /*_*/page_assessments_projects (pap_project_title);
