3fccd0e177801f0fd9f7a41c2828ca7508b14a68
MusikAnimal <musikanimal@gmail.com>
2023-01-29 20:08:05 -0500

    PageAssessments.i18n.alias.php: add alias for ne
    Bug: T328224
    Change-Id: Ibb83bfc6a836ad8a94304b1e32fe69922df06302

--- PageAssessments.i18n.alias.php
+++ PageAssessments.i18n.alias.php
@@ -22,6 +22,11 @@ $specialPageAliases['ko'] = [
 	'PageAssessments' => [ '문서평가' ],
 ];
 
+/** नेपाली (Nepali) */
+$specialPageAliases['ne'] = [
+	'PageAssessents' => [ 'पृष्ठ_मूल्याङ्कन' ],
+];
+
 /** 中文 (Chinese) */
 $specialPageAliases['zh'] = [
 	'PageAssessments' => [ '页面评级', '頁面評級' ],
