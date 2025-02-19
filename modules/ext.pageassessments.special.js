0cc0d5fb625111f6793bb1a3d58b63c6fd9d7bec
Ed Sanders <esanders@wikimedia.org>
2025-02-19 15:43:58 +0000

    ESLint: Manually fix prefer-const violations
    Change-Id: Iea006f7740f585bb283e85460cebdc63805c8c9b

--- modules/ext.pageassessments.special.js
+++ modules/ext.pageassessments.special.js
@@ -6,18 +6,17 @@
 	// eslint-disable-next-line no-jquery/no-global-selector
 	$( 'input[name="page_title"]' ).suggestions( {
 		fetch: function ( userInput, response, maxRows ) {
-			let apiParams, request,
-				node = this[ 0 ],
+			const node = this[ 0 ],
 				// eslint-disable-next-line no-jquery/no-global-selector
 				namespace = OO.ui.infuse( $( '#pageassessments-namespace' ) ),
 				api = new mw.Api();
-			apiParams = {
+			const apiParams = {
 				action: 'opensearch',
 				namespace: namespace.getValue(),
 				search: userInput,
 				limit: maxRows
 			};
-			request = api.get( apiParams )
+			const request = api.get( apiParams )
 				.done( ( data ) => {
 					response( data[ 1 ] );
 				} );
@@ -39,10 +38,9 @@
 	// eslint-disable-next-line no-jquery/no-global-selector
 	$( 'input[name="project"]' ).suggestions( {
 		fetch: function ( userInput, response, maxRows ) {
-			let matchingProjects,
-				allProjects = mw.config.get( 'wgPageAssessmentProjects' ) || [];
+			const allProjects = mw.config.get( 'wgPageAssessmentProjects' ) || [];
 
-			matchingProjects = allProjects.filter( ( value ) => value.slice( 0, Math.max( 0, userInput.length ) ).toLocaleLowerCase() === userInput.toLocaleLowerCase() );
+			const matchingProjects = allProjects.filter( ( value ) => value.slice( 0, Math.max( 0, userInput.length ) ).toLocaleLowerCase() === userInput.toLocaleLowerCase() );
 
 			response( matchingProjects.slice( 0, maxRows ) );
 		}
